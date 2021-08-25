8a11dac73998d436a7330bd7505c896440bf4512
sajunius <45623924+sajunius@users.noreply.github.com>
2021-08-25 12:16:27 +0200

    Incresed precision of x values
    Increasing precision of x values to ensure that the whole range that acropolis runs over is covered. This was previously not always the case due to rounding errors

--- Acropolis_scripts/number_density_script.m
+++ Acropolis_scripts/number_density_script.m
@@ -2,18 +2,24 @@
 
 
 (*Reading the parameter file*)
-Get["~/prj/acropolis/param.m"];
-{m1, m2, mZp, tant, eps, ax} = ToExpression[StringReplace[#, {"e+" :> "*^", "e-" :> "*^-"}]] & /@ {m1, m2, mZp, tant, eps, ax};
-(*I am rounding the numbers to get exact number instead of floats. This is necesarry to obtain enough precision for the Boltzmann code*)
-ModelParam = {aX -> Round[ax*10^15]*10^-15, epsilon -> Round[eps*10^15]*10^-15, ttheta -> Round[tant*10^15]*10^-15, MZp -> Round[mZp*10^15]*10^-15, MD1 -> Round[m1*10^15]*10^-15, MD2 -> Round[m2*10^15]*10^-15, gD1 -> 2, gD2 -> 2};
-Print["ModelParam=",ModelParam];
+Get["param.m"];
 
-SetDirectory["~/prj/coscattering/Sam/MathematicaNotebooks/"];
+m1=ToExpression[StringReplace[m1,"e"->" 10^"]];
+m2=ToExpression[StringReplace[m2,"e"->" 10^"]];
+mZp=ToExpression[StringReplace[mZp,"e"->" 10^"]];
+ax=ToExpression[StringReplace[ax,"e"->" 10^"]];
+eps=ToExpression[StringReplace[eps,"e"->" 10^"]];
+tant=ToExpression[StringReplace[tant,"e"->" 10^"]];
+
+(*I am rounding the numbers to get exact number instead of floats. This is necesarry to obtain enough precision for the Boltzmann code*)
+ModelParam = {aX -> Round[ax*10^5]*10^-5, epsilon -> Round[eps*10^5]*10^-5, ttheta -> Round[tant*10^5]*10^-5, MZp -> Round[mZp*10^5]*10^-5, MD1 -> Round[m1*10^5]*10^-5, MD2 -> Round[m2*10^5]*10^-5, gD1 -> 2, gD2 -> 2};
+
+SetDirectory["/home/sam/OneDrive/Coscattering/"];
 Get["Coscattering.wl"];
 
 res=DMabundance[ModelParam,xmin,xmax];
-Print["res=",res];
+Print[res];
 (*Exporting the mediator number density to a file*)
-xval = 10^Range[Log10[xmin], Log10[xmax], (Log10[xmax] - Log10[xmin])/49]//N;
+xval = N[10^Range[Log10[xmin], Log10[xmax], (Log10[xmax] - Log10[xmin])/49],10];
 nMed = Map[Ymed, xval]*Map[Entr, MD1/xval //. ModelParam]//N;
-Export["~/prj/acropolis/number_density.dat", Transpose[{xval, nMed}]];
+Export["/home/sam/Software/acropolis/number_density.dat", Transpose[{xval, nMed}]];
